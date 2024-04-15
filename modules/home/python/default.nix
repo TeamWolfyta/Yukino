458b449b87af434c2d5e50f5b9e439ff8b58f51b
TeamWolfyta <kieran@wolfyta.dev>
2024-04-15 19:02:16 +0100

    feat: add (hopefully) python3 along with remgb

---
+++ modules/home/python/default.nix
@@ -0,0 +1,20 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+with lib;
+with lib.yukino;
+let
+  _config = config.yukino.python;
+in
+{
+  options.yukino.python = with types; {
+    enable = mkBoolOpt false "yukino.python.enable";
+  };
+
+  config = mkIf _config.enable {
+    home.packages = [ (pkgs.python3.withPackages (python-pkgs: [ python-pkgs."rembg[cli]" ])) ];
+  };
+}
