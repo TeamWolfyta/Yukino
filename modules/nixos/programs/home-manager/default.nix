1a17559fd1c4510b7813c8597f9a35cc3ac0c1ce
TeamWolfyta <kieran@wolfyta.dev>
2024-04-21 12:07:43 +0100

    feat: add the ability to specify options that get passed directly to home manager from nixos modules

--- modules/nixos/programs/home-manager/default.nix
+++ modules/nixos/programs/home-manager/default.nix
@@ -3,11 +3,15 @@ with lib;
 with lib.yukino;
 let
   _config = config.yukino.programs.home-manager;
+  user = config.yukino.config.user;
 in
 {
-  options.yukino.programs.home-manager = { };
+  options.yukino.programs.home-manager = {
+    extraOptions = mkOpt attrs { } "yukino.programs.home-manager.extraOptions";
+  };
 
   config = {
+    snowfallorg.users.${user.name}.home.config = _config.extraOptions;
     home-manager = {
       useUserPackages = true;
       useGlobalPkgs = true;
