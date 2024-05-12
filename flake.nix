76d0e916f620d37238cfb7e71b3eb145bc604e21
TeamWolfyta <kieran@wolfyta.dev>
2024-05-12 19:50:22 +0100

    style: remove use of objects

--- flake.nix
+++ flake.nix
@@ -4,25 +4,19 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
+    nur.url = "github:nix-community/NUR";
+
     nixos-wsl.url = "github:nix-community/nixos-wsl";
     nixos-hardware.url = "github:NixOS/nixos-hardware";
 
-    nur.url = "github:nix-community/NUR";
+    snowfall-lib.url = "github:snowfallorg/lib";
+    snowfall-lib.inputs.nixpkgs.follows = "nixpkgs";
 
-    snowfall-lib = {
-      url = "github:snowfallorg/lib";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
-    nix-ld-rs = {
-      url = "github:nix-community/nix-ld-rs";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    nix-ld-rs.url = "github:nix-community/nix-ld-rs";
+    nix-ld-rs.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs:
